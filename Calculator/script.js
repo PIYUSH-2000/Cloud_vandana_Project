149197e53204723b96eac3cbe0b12bb95a0b8aa7
PIYUSH-2000 <103765408+PIYUSH-2000@users.noreply.github.com>
2023-11-21 16:22:16 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,19 @@
+// Function to append the clicked button value to the display
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+// Function to calculate the result
+function calculateResult() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
 
