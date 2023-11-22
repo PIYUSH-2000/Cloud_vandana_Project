a0884aa1767a37b7d0afd2e8145a732a757265bd
PIYUSH-2000 <103765408+PIYUSH-2000@users.noreply.github.com>
2023-11-22 09:48:21 +0530

    Update PangramChecker.java

--- java/PangramChecker.java
+++ java/PangramChecker.java
@@ -1,1 +1,30 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class PangramChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String input = scanner.nextLine().toLowerCase();
+
+        boolean isPangram = checkPangram(input);
+
+        if (isPangram) {
+            System.out.println("The input is a pangram.");
+        } else {
+            System.out.println("The input is not a pangram.");
+        }
+    }
+
+    public static boolean checkPangram(String input) {
+        Set<Character> uniqueLetters = new HashSet<>();
+        for (char ch : input.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                uniqueLetters.add(ch);
+            }
+        }
+        return uniqueLetters.size() == 26;
+    }
+}
