75da124526053bdcc81e0de95b3507b3f4c1abfa
PIYUSH-2000 <103765408+PIYUSH-2000@users.noreply.github.com>
2023-11-22 10:25:53 +0530

    Update sorting.js

--- JavaScript/sorting.js
+++ JavaScript/sorting.js
@@ -1,1 +1,24 @@
+function bubbleSort(arr) {
+    var n = arr.length;
+    var swapped;
 
+    do {
+        swapped = false;
+
+        for (var i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                var temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        n--;
+    } while (swapped);
+    return arr;
+}
+var unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+console.log("Unsorted Array: " + unsortedArray);
+
+var sortedArray = bubbleSort(unsortedArray.slice());
+console.log("Sorted Array: " + sortedArray);
